b5f4a74c34acc958224f1f28fcf2ff7feaad7b17
root <Olatunde Pedro>
2022-10-21 02:34:57 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - Print numbers, from 0 to 9
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int i = 38;
+
+	while (i <= 47)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
